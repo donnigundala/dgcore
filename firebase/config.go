53345942b68548163c4ba7c702b6ec5166f97e8d
Donni <donnigundala@gmail.com>
2025-10-27 15:05:32 +0700

    ref: firebase add support ot credentials file or json string

--- firebase/config.go
+++ firebase/config.go
@@ -2,5 +2,8 @@ package firebase
 
 // Config struct for Firebase
 type Config struct {
+	// CredentialsFile path to the JSON credentials file.
 	CredentialsFile string `mapstructure:"credentials_file" json:"credentials_file" yaml:"credentials_file"`
+	// CredentialsJSON json string of the credentials.
+	CredentialJSON string `mapstructure:"credential_json" json:"credential_json" yaml:"credential_json"`
 }
--- firebase/firbase.go
+++ firebase/firbase.go
@@ -2,6 +2,7 @@ package firebase
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"os"
 
@@ -27,17 +28,7 @@ func WithLogger(logger *slog.Logger) Option {
 
 // NewFirebase initializes and returns a new Firebase instance.
 func NewFirebase(ctx context.Context, cfg *Config, opts ...Option) (*Firebase, error) {
-	opt := option.WithCredentialsFile(cfg.CredentialsFile)
-
-	app, err := firebase.NewApp(ctx, nil, opt)
-	if err != nil {
-		// Temporary logger for initialization errors
-		tempLogger := slog.New(slog.NewTextHandler(os.Stderr, nil)).With("component", "firebase")
-		tempLogger.Error("Failed to initialize Firebase app", "error", err)
-		return nil, err
-	}
-
-	fb := &Firebase{App: app}
+	fb := &Firebase{}
 
 	for _, opt := range opts {
 		opt(fb)
@@ -46,8 +37,29 @@ func NewFirebase(ctx context.Context, cfg *Config, opts ...Option) (*Firebase, e
 	if fb.logger == nil {
 		fb.logger = slog.New(slog.NewTextHandler(os.Stderr, nil))
 	}
-
 	fb.logger = fb.logger.With("component", "firebase")
+
+	var credOption option.ClientOption
+
+	if cfg.CredentialJSON != "" {
+		fb.logger.Info("Initializing Firebase with credentials from firebase.credentials_json environment variable")
+		credOption = option.WithCredentialsJSON([]byte(cfg.CredentialJSON))
+	} else if cfg.CredentialsFile != "" {
+		fb.logger.Info("Initializing Firebase with credentials file", "path", cfg.CredentialsFile)
+		credOption = option.WithCredentialsFile(cfg.CredentialsFile)
+	} else {
+		err := errors.New("firebase credentials not found. Set firebase.credentials_json or firebase.credentials_file in config")
+		fb.logger.Error(err.Error())
+		return nil, err
+	}
+
+	app, err := firebase.NewApp(ctx, nil, credOption)
+	if err != nil {
+		fb.logger.Error("Failed to initialize Firebase app", "error", err)
+		return nil, err
+	}
+
+	fb.App = app
 	fb.logger.Info("Initialized successfully")
 
 	return fb, nil
