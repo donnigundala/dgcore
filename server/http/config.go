e54dadb701a3bba3ab42a00fa5db28a373c24ff6
Donni <donnigundala@gmail.com>
2025-11-11 01:36:19 +0700

    chore: update http server to follow DI concept

--- server/http/config.go
+++ server/http/config.go
@@ -2,18 +2,16 @@ package http
 
 import (
 	"time"
-
-	"github.com/donnigundala/dgcore/config"
 )
 
 // Config holds all configuration for the HTTP server.
 type Config struct {
-	Host        string        `mapstructure:"host"`
-	Port        string        `mapstructure:"port"`
-	ReadTimeout time.Duration `mapstructure:"read_timeout"`
+	Host         string        `mapstructure:"host"`
+	Port         string        `mapstructure:"port"`
+	ReadTimeout  time.Duration `mapstructure:"read_timeout"`
 	WriteTimeout time.Duration `mapstructure:"write_timeout"`
-	IdleTimeout time.Duration `mapstructure:"idle_timeout"`
-	TLS         TLSConfig     `mapstructure:"tls"`
+	IdleTimeout  time.Duration `mapstructure:"idle_timeout"`
+	TLS          TLSConfig     `mapstructure:"tls"`
 }
 
 // TLSConfig holds the TLS-specific configuration.
@@ -22,20 +20,4 @@ type TLSConfig struct {
 	CertFile   string `mapstructure:"cert_file"`
 	KeyFile    string `mapstructure:"key_file"`
 	TLSVersion string `mapstructure:"tls_version"`
-}
-
-// init registers the default configuration values with the global config manager.
-// This allows the application to have sensible defaults and discoverable settings.
-func init() {
-	config.Add("server.http", map[string]any{
-		"host":         "127.0.0.1",
-		"port":         "8080",
-		"read_timeout": "5s",
-		"write_timeout": "10s",
-		"idle_timeout": "120s",
-		"tls.enabled":    false,
-		"tls.cert_file":  "",
-		"tls.key_file":   "",
-		"tls.tls_version": "TLS1.2",
-	})
 }
